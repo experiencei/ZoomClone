d42202474b6beb33512b6d1f108c101d1a097013
experiencei <ayelojahighbee01@gmail.com>
2021-11-21 09:42:57 +0100

    working on the meetings screen

--- components/ContactsMenu.js
+++ components/ContactsMenu.js
@@ -43,7 +43,7 @@ const ContactsMenu = () => {
                         {contact.name}
                     </Text>
             </View>
-        )}
+        )} 
             
         </View>
     )
