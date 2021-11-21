1781bf19280f02cf604b53c24357ff858f4cdaea
experiencei <ayelojahighbee01@gmail.com>
2021-11-21 11:36:58 +0100

    adding server to the frontend

---
+++ components/StartMeeting.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const StartMeeting = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default StartMeeting
+
+const styles = StyleSheet.create({})
