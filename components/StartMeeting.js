2d62996a1812b73daafeeba096420378855a21bc
experiencei <ayelojahighbee01@gmail.com>
2021-11-21 11:40:32 +0100

    adding server to the frontend

--- components/StartMeeting.js
+++ components/StartMeeting.js
@@ -1,14 +1,67 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+
 
 const StartMeeting = () => {
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <View style={styles.startMeetingContainer}>
+                 <View style={styles.info}>
+                     <TextInput 
+                         style={styles.textInput}
+                         value={name}
+                         placeholder="Enter name"
+                         onChangeText={text => setName(text)}
+                         placeholderTextColor="#767476"
+                     />
+                 </View>
+                 <View style={styles.info}>
+                    <TextInput 
+                            style={styles.textInput}
+                            value={roomId}
+                            placeholder="Enter Room Id"
+                            onChangeText={text => setRoomId(text)}
+                            placeholderTextColor="#767476"
+                        />
+                 </View>
+                 <View style={{alignItems: 'center'}}>
+                      <TouchableOpacity 
+                      onPress={() =>{}}
+                      style={styles.startMeetingButton}
+                      >
+                      <Text style={{color : "white" , fontWeight : "bold"}}>Start Meeting</Text>
+                      </TouchableOpacity>
+                 </View>
+            </View>
     )
 }
 
 export default StartMeeting
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    startMeetingContainer : {
+
+    },
+    info : {
+        width : "100%",
+        backgroundColor : "#373538",
+        height: 50 ,
+        borderBottomWidth : 1,
+        borderTopWidth : 1,
+        borderColor : "#484648",
+        padding : 12,
+       justifyContent : "center",
+    },
+    textInput:{
+        color: "white",
+        fontSize : 18
+    },
+    startMeetingButton : {
+        width: 350,
+        marginTop : 20,
+        justifyContent : "center",
+        alignItems : "center",
+        backgroundColor : "#0470DC",
+        height: 50,
+        borderRadius : 15,
+    }
+})
--- screens/MeetingRoom.js
+++ screens/MeetingRoom.js
@@ -1,39 +1,13 @@
 import React , {useState} from 'react'
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import StartMeeting from '../components/StartMeeting'
 
 const MeetingRoom = () => {
   const [name , setName] = useState('')
   const [roomId , setRoomId] = useState('')
     return (
         <View style={styles.container}>
-            <View style={styles.startMeetingContainer}>
-                 <View style={styles.info}>
-                     <TextInput 
-                         style={styles.textInput}
-                         value={name}
-                         placeholder="Enter name"
-                         onChangeText={text => setName(text)}
-                         placeholderTextColor="#767476"
-                     />
-                 </View>
-                 <View style={styles.info}>
-                    <TextInput 
-                            style={styles.textInput}
-                            value={roomId}
-                            placeholder="Enter Room Id"
-                            onChangeText={text => setRoomId(text)}
-                            placeholderTextColor="#767476"
-                        />
-                 </View>
-                 <View style={{alignItems: 'center'}}>
-                      <TouchableOpacity 
-                      onPress={() =>{}}
-                      style={styles.startMeetingButton}
-                      >
-                      <Text style={{color : "white" , fontWeight : "bold"}}>Start Meeting</Text>
-                      </TouchableOpacity>
-                 </View>
-            </View>
+            <StartMeeting/>
         </View>
     )
 }
@@ -45,30 +19,4 @@ const styles = StyleSheet.create({
          backgroundColor : "#1c1c1c",
          flex: 1,
     },
-    startMeetingContainer : {
-
-    },
-    info : {
-        width : "100%",
-        backgroundColor : "#373538",
-        height: 50 ,
-        borderBottomWidth : 1,
-        borderTopWidth : 1,
-        borderColor : "#484648",
-        padding : 12,
-       justifyContent : "center",
-    },
-    textInput:{
-        color: "white",
-        fontSize : 18
-    },
-    startMeetingButton : {
-        width: 350,
-        marginTop : 20,
-        justifyContent : "center",
-        alignItems : "center",
-        backgroundColor : "#0470DC",
-        height: 50,
-        borderRadius : 15,
-    }
 })
