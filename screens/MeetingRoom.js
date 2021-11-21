8dc20efd271c0f4a9ea5144e2825ee1f8fccb5b8
experiencei <ayelojahighbee01@gmail.com>
2021-11-21 10:51:46 +0100

    working on the meetings screen

--- screens/MeetingRoom.js
+++ screens/MeetingRoom.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React , {useState} from 'react'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
 
 const MeetingRoom = () => {
+  const [name , setName] = useState('')
+
     return (
-        <View>
+        <View style={styles.container}>
             <View style={styles.startMeetingContainer}>
                  <View style={styles.info}>
-
+                     <TextInput 
+                         style={styles.textInput}
+                         value={name}
+                         onChangeText={text => setName(text)}
+                     />
                  </View>
                  <View style={styles.info}>
 
@@ -20,7 +26,8 @@ export default MeetingRoom
 
 const styles = StyleSheet.create({
     container: {
-
+         backgroundColor : "#1c1c1c",
+         flex: 1,
     },
     startMeetingContainer : {
 
