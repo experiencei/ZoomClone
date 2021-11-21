b5d786263a08df339f6d927e78ae40482a4f045e
experiencei <ayelojahighbee01@gmail.com>
2021-11-21 07:46:56 +0100

    working on the Home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,19 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View ,SafeAreaView } from 'react-native'
+import ContactsMenu from '../components/ContactsMenu'
 import Header from '../components/Header'
+import MenuButtons from '../components/MenuButtons'
 import SearchBar from '../components/SearchBar'
 
 const Home = () => {
     return (
         <View>
+        <SafeAreaView>
             <Header/>
             <SearchBar/>
+            <MenuButtons/>
+            <ContactsMenu/>
+        </SafeAreaView>
         </View>
     )
 }
