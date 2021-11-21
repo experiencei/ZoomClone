674f4b33a7a2feac597b349d90a6f022fc68cc2e
experiencei <ayelojahighbee01@gmail.com>
2021-11-21 08:04:27 +0100

    working on the Home screen

--- components/Header.js
+++ components/Header.js
@@ -5,6 +5,8 @@ const Header = () => {
     return (
         <View style={styles.container}>
             <Entypo name="notification" size={30} color="#efefef" />
+            <Text style={styles.heading}>Meet & Chat</Text>
+            <Entypo name="new-message" size={30} color="#efefef" />
         </View>
     )
 }
@@ -13,6 +15,11 @@ export default Header
 
 const styles = StyleSheet.create({
   container: {
-      
+
+  },
+  heading : {
+      color : "#efefef",
+      fontSize : 20,
+      fontWeight : "700",
   }
 })
